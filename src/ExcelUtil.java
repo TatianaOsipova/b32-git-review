569db67ec83a92055733dd7abed4301224a7f0cc
Tatiana Osipova <otv120780@gmail.com>
2024-01-20 15:43:26 -0500

    create method3

--- src/ExcelUtil.java
+++ src/ExcelUtil.java
@@ -2,8 +2,10 @@ public class ExcelUtil {
     public void method1(){
         // read cell
     }
-
     public void method2(){
+        // write to cell
+    }
+    public void method3(){
         // write to cell
     }
 }
